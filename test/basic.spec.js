0c3953a5457ad77b312fe6e0e8b3a3fbc120021c
Brian Ford <btford@umich.edu>
2014-04-04 23:52:23 -0700

    test: check that spacing is preserved

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -6,4 +6,8 @@ describe('highlight', function () {
     expect(highlight('var x = 42'))
         .toBe('<pre><div class="line"><span class="keyword">var</span> <span class="identifier">x</span> = <span class="numeric">42</span></div></pre>');
   });
+  it('should preserve spacing', function () {
+    expect(highlight('var   x   = 42'))
+        .toBe('<pre><div class="line"><span class="keyword">var</span>   <span class="identifier">x</span>   = <span class="numeric">42</span></div></pre>');
+  });
 });
